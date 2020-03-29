574fc0ed409b51d2080a0c02c10efcb17787b85c
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-03-29 14:24:00 -0300

    Initial Commit

---
+++ BMOOpenCV-06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BMOOpenCV-06
+//
+//  Created by Breno Medeiros on 29/03/20.
+//  Copyright © 2020 ProgramasBMO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
